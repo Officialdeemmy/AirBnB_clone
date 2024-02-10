47c8cbc3f697cd4cad0ae1eced972115592d1f5a
Ademola <adenijiademolaafis@gmail.com>
2024-02-09 16:58:10 -0800

    qriting base models and console

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+hajar write here
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Defines amenities that user can choose from to offer at its place"""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+hajar write here
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class to create a state"""
+    name = ""
