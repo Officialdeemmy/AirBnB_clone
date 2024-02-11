66e744c6ad7a925f82778a2af57d7d80cec5e52d
moumkinehazar <141556493+moumkinehazar@users.noreply.github.com>
2024-02-11 15:42:34 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-State Module for HBNB project
-"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """class to create a state"""
+    """ State class """
     name = ""
